f128ce809f8578e01944b6d9b5d526999ea05a9a
malik000111 <malickahmar4@gmail.com>
2026-02-24 14:05:07 +0600

    Add explanation of Java concepts learned
    Document learning experiences with inheritance, method overriding, and encapsulation in Java.

---
+++ ExplanationTask.java
@@ -0,0 +1,5 @@
+ In this assignment, i learned in what way inheritance works in Java.Firstly , I created a main BankAccount class and then made SavingsAccount and CheckingAccount from it using extends. It is because to understand how   code can be reused instead of writing everything again.
+
+I also learned about method overriding. The child classes can change some behavior from the parent class, like adding interest or handling overdraft. This showed me how subclasses can be little different but still connected to the main class.
+
+Another thing I learned is encapsulation. Making variables private and using public methods keeps the data safe. Other thing i understand is that, why  checking user input is important, so balance does not become negative by mistake.
